81536501509f4e482680bee44e096a79b8fb808e
Vinay <83056464+vinaycreative@users.noreply.github.com>
2026-01-06 20:21:56 +0530

    Add files via upload

--- src/types/express.ts
+++ src/types/express.ts
@@ -2,6 +2,19 @@ import { JwtPayload } from "../config/jwt"
 
 declare global {
   namespace Express {
+    namespace Multer {
+      interface File {
+        fieldname: string
+        originalname: string
+        encoding: string
+        mimetype: string
+        size: number
+        destination: string
+        filename: string
+        path: string
+        buffer: Buffer
+      }
+    }
     interface Request {
       user?: JwtPayload
     }
